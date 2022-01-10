6756b0d7045dd04a7072ef1d1bea44ecee5840ce
Alverd04 <albert.font.fernandez@gmail.com>
2022-01-10 21:45:31 +0100

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Albert Font Fernández on 10/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
